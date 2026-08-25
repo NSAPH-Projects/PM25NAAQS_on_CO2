7f73c0cbef049ccd65d0ed86105fc889698d8d49
Veronica Ballerini <115488016+veronicaballerini@users.noreply.github.com>
2026-08-25 11:10:25 +0200

    Update 07c_naturalgasshifting.R

--- 07c_naturalgasshifting.R
+++ 07c_naturalgasshifting.R
@@ -17,45 +17,45 @@ project.dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(project.dir)
 
 # 1. Read data
-fracking<-read.table("increase_naturalgas.txt")
+naturalgasshifting<-read.table("increase_naturalgas.txt")
 load(paste(project.dir,"/main_analysis.RData",sep=""))
 
 # 2. Meta-analysis for each period
 
-df2009$fracking<-ifelse(df2009$State%in%fracking$State[which(fracking$fracking2009==1)],1,0)
-df2012$fracking<-ifelse(df2012$State%in%fracking$State[which(fracking$fracking2012==1)],1,0)
-df2014$fracking<-ifelse(df2014$State%in%fracking$State[which(fracking$fracking2014==1)],1,0)
+df2009$naturalgasshifting<-ifelse(df2009$State%in%naturalgasshifting$State[which(naturalgasshifting$naturalgasshifting2009==1)],1,0)
+df2012$naturalgasshifting<-ifelse(df2012$State%in%naturalgasshifting$State[which(naturalgasshifting$naturalgasshifting2012==1)],1,0)
+df2014$naturalgasshifting<-ifelse(df2014$State%in%naturalgasshifting$State[which(naturalgasshifting$naturalgasshifting2014==1)],1,0)
 
-reg_2009_fr <- rma(yi = cum_effect, sei = sd,  method = "REML", mods = ~ as.factor(fracking), 
+reg_2009_fr <- rma(yi = cum_effect, sei = sd,  method = "REML", mods = ~ as.factor(naturalgasshifting), 
                    data = df2009)
 s_reg2009_fr <- summary(reg_2009_fr)
 
-reg_2009_rel_fr <- rma(yi = cum_effect_rel, sei = sd_rel,  method = "REML", mods = ~ as.factor(fracking), 
+reg_2009_rel_fr <- rma(yi = cum_effect_rel, sei = sd_rel,  method = "REML", mods = ~ as.factor(naturalgasshifting), 
                    data = df2009)
 s_reg2009_rel_fr <- summary(reg_2009_rel_fr)
 
-reg_2012_fr <- rma(yi = cum_effect, sei = sd,  method = "REML", mods = ~ as.factor(fracking), 
+reg_2012_fr <- rma(yi = cum_effect, sei = sd,  method = "REML", mods = ~ as.factor(naturalgasshifting), 
                    data = df2012)
 s_reg2012_fr <- summary(reg_2012_fr)
 
-reg_2012_rel_fr <- rma(yi = cum_effect_rel, sei = sd_rel,  method = "REML", mods = ~ as.factor(fracking), 
+reg_2012_rel_fr <- rma(yi = cum_effect_rel, sei = sd_rel,  method = "REML", mods = ~ as.factor(naturalgasshifting), 
                        data = df2012)
 s_reg2012_rel_fr <- summary(reg_2012_rel_fr)
 
-reg_2014_fr <- rma(yi = cum_effect, sei = sd,  method = "REML", mods = ~ as.factor(fracking), 
+reg_2014_fr <- rma(yi = cum_effect, sei = sd,  method = "REML", mods = ~ as.factor(naturalgasshifting), 
                    data = df2014)
 s_reg2014_fr <- summary(reg_2014_fr)
 
-reg_2014_rel_fr <- rma(yi = cum_effect_rel, sei = sd_rel,  method = "REML", mods = ~ as.factor(fracking), 
+reg_2014_rel_fr <- rma(yi = cum_effect_rel, sei = sd_rel,  method = "REML", mods = ~ as.factor(naturalgasshifting), 
                        data = df2014)
 s_reg2014_rel_fr <- summary(reg_2014_rel_fr)
 
-saveRDS(s_res2009, file = paste0(project.dir,"/main_results/meta_analysis_20052009_fracking.rds"))
-saveRDS(s_res2012, file = paste0(project.dir,"/main_results/meta_analysis_20052012_fracking.rds"))
-saveRDS(s_res2014, file = paste0(project.dir,"/main_results/meta_analysis_20052014_fracking.rds"))
-saveRDS(s_res2009_rel, file = paste0(project.dir,"/main_results/meta_analysis_rel_20052009_fracking.rds"))
-saveRDS(s_res2012_rel, file = paste0(project.dir,"/main_results/meta_analysis_rel_20052012_fracking.rds"))
-saveRDS(s_res2014_rel, file = paste0(project.dir,"/main_results/meta_analysis_rel_20052014_fracking.rds"))
+saveRDS(s_res2009, file = paste0(project.dir,"/main_results/meta_analysis_20052009_naturalgasshifting.rds"))
+saveRDS(s_res2012, file = paste0(project.dir,"/main_results/meta_analysis_20052012_naturalgasshifting.rds"))
+saveRDS(s_res2014, file = paste0(project.dir,"/main_results/meta_analysis_20052014_naturalgasshifting.rds"))
+saveRDS(s_res2009_rel, file = paste0(project.dir,"/main_results/meta_analysis_rel_20052009_naturalgasshiftingg.rds"))
+saveRDS(s_res2012_rel, file = paste0(project.dir,"/main_results/meta_analysis_rel_20052012_naturalgasshifting.rds"))
+saveRDS(s_res2014_rel, file = paste0(project.dir,"/main_results/meta_analysis_rel_20052014_naturalgasshifting.rds"))
 
 # 3. Add year and combine state results for different time frames
 df2009$Year <- "2005-2009"
@@ -64,9 +64,9 @@ df2014$Year <- "2005-2014"
 df_reg <- rbind(df2009, df2012, df2014)
 
 # 4. Join state-level meta-data
-df_fracking <- left_join(df_reg, fracking[, c(1,4)], by = "State")
-df_fracking$fracking <- as.factor(df_fracking$fracking2014)
-levels(df_fracking$fracking) <- c("No effect", "Significant effect")
+df_naturalgasshifting <- left_join(df_reg, naturalgasshifting[, c(1,4)], by = "State")
+df_naturalgasshifting$naturalgasshifting <- as.factor(df_naturalgasshifting$naturalgasshifting2014)
+levels(df_naturalgasshifting$naturalgasshifting) <- c("No effect", "Significant effect")
 
 # 5. Extract group means
 
@@ -75,16 +75,16 @@ extract_group_meta <- function(m.rma, year_label) {
   eff0 <- cf["intrcpt", "estimate"]
   se0  <- cf["intrcpt", "se"]
   ci0  <- c(cf["intrcpt", "ci.lb"], cf["intrcpt", "ci.ub"])
-  eff1 <- eff0 + cf[grep("fracking", rownames(cf)), "estimate"]
+  eff1 <- eff0 + cf[grep("naturalgasshifting", rownames(cf)), "estimate"]
   # This is a conservative (over-)estimate of SE; for display only
-  se1  <- sqrt(se0^2 + cf[grep("fracking", rownames(cf)), "se"]^2)
+  se1  <- sqrt(se0^2 + cf[grep("naturalgasshifting", rownames(cf)), "se"]^2)
   ci1  <- c(
-    cf["intrcpt", "estimate"] + cf[grep("fracking", rownames(cf)), "ci.lb"],
-    cf["intrcpt", "estimate"] + cf[grep("fracking", rownames(cf)), "ci.ub"]
+    cf["intrcpt", "estimate"] + cf[grep("naturalgasshifting", rownames(cf)), "ci.lb"],
+    cf["intrcpt", "estimate"] + cf[grep("naturalgasshifting", rownames(cf)), "ci.ub"]
   )
   data.frame(
     State = c("Group mean1","Group mean2"),
-    fracking = c("No effect", "Significant effect"),
+    naturalgasshifting = c("No effect", "Significant effect"),
     mean = c(eff0, eff1),
     lower = c(ci0[1], ci1[1]),
     upper = c(ci0[2], ci1[2]),
@@ -102,36 +102,36 @@ meta_points <- bind_rows(
 # 6. Construct plot dataset
 
 # Prepare state-level summary for plotting
-summary_df <- df_fracking %>%
+summary_df <- df_naturalgasshifting %>%
   mutate(
     lower = cum_effect_rel - 1.96 * sd_rel,
     upper = cum_effect_rel + 1.96 * sd_rel
   ) %>%
-  select(State, mean = cum_effect_rel, lower, upper, Year, fracking)
+  select(State, mean = cum_effect_rel, lower, upper, Year, naturalgasshifting)
 
 # Bind meta-analytic group-level means as if they were states
 plot_data <- summary_df %>%
   filter(State != "U.S.") %>% # Remove US if present (optional)
   bind_rows(meta_points) %>%
-  mutate(fracking = factor(fracking, levels = c("No effect","Significant effect")))
+  mutate(naturalgasshifting = factor(naturalgasshifting, levels = c("No effect","Significant effect")))
 
 # 7. Sorting States (effect 2005-2012)
 
 state_order_table <- plot_data %>%
   filter(Year == "2005-2012", State != "Average effects") %>%
-  group_by(fracking) %>%
+  group_by(naturalgasshifting) %>%
   arrange(mean) %>%
   summarise(state_levels = list(c(unique(State), "Average effects")), .groups = "drop")
 
 plot_data <- plot_data %>%
-  left_join(state_order_table, by = "fracking") %>%
-  group_by(fracking) %>%
+  left_join(state_order_table, by = "naturalgasshifting") %>%
+  group_by(naturalgasshifting) %>%
   mutate(State_ord = factor(State, levels = state_levels[[1]])) %>%
   ungroup() %>%
   select(-state_levels)
 
 plot_data$panel_style <- ifelse(
-  plot_data$fracking == "No effect",
+  plot_data$naturalgasshifting == "No effect",
   "No effect",
   "Significant effect"
 )
@@ -139,14 +139,14 @@ plot_data$panel_style <- ifelse(
 # 8. Create a separate third panel for group means
 
 # create a panel variable with 3 panels
-plot_data$fracking_panel <- ifelse(
+plot_data$naturalgasshifting_panel <- ifelse(
   plot_data$State %in% c("Group mean1","Group mean2"),
   "Average effects",
-  as.character(plot_data$fracking)
+  as.character(plot_data$naturalgasshifting)
 )
 
-plot_data$fracking_panel <- factor(
-  plot_data$fracking_panel,
+plot_data$naturalgasshifting_panel <- factor(
+  plot_data$naturalgasshifting_panel,
   levels = c(
     "No effect",
     "Significant effect",
@@ -158,13 +158,13 @@ plot_data$fracking_panel <- factor(
 state_order_table <- plot_data %>%
   filter(Year == "2005-2012",
          !(State %in% c("Group mean1","Group mean2"))) %>%
-  group_by(fracking) %>%
+  group_by(naturalgasshifting) %>%
   arrange(mean) %>%
   summarise(state_levels = list(unique(State)), .groups = "drop")
 
 # 10. Apply ordering only to real states
 plot_data <- plot_data %>%
-  left_join(state_order_table, by = "fracking") %>%
+  left_join(state_order_table, by = "naturalgasshifting") %>%
   mutate(
     State_ord =
       ifelse(State %in% c("Group mean1","Group mean2"),
@@ -239,7 +239,7 @@ ggplot(plot_data, aes(x = State_ord, y = mean, color = Year, group = Year)) +
     # )
   )+
   facet_grid2(
-    ~ fracking_panel,
+    ~ naturalgasshifting_panel,
     scales = "free_x",
     space = "free",
     shrink = TRUE,
